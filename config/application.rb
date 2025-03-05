4e9a8a5dee9e53b18b798738e13f5019fc7ecc7d
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-03-05 22:01:42 +0900

    second commit

--- config/application.rb
+++ config/application.rb
@@ -24,4 +24,7 @@ module ManagingApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
+  config.hosts << 'xxx.onrender.com'
+  
 end
