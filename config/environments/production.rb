c8514834bfbad94077ff2f675605a83f328d8a8f
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-03-05 22:11:26 +0900

    4th

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.config.hosts << "managing-app.onrender.com"
+
+  
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
