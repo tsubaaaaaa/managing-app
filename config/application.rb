16f567e138d20b0d2c6fe91c8fbe8f8e1590a3eb
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-03-05 22:05:27 +0900

    3rd commit

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module ManagingApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+
+    config.hosts << 'xxx.onrender.com' #render.com
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -25,6 +28,4 @@ module ManagingApp
     # config.eager_load_paths << Rails.root.join("extras")
   end
 
-  config.hosts << 'xxx.onrender.com'
-  
 end
