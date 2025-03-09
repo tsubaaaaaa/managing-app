9bd40aaf36e89de2a5d16eed1b8a1634c4b78210
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-03-09 17:35:59 +0900

    19th

--- db/migrate/20250302064038_add_species_to_infos.rb
+++ db/migrate/20250302064038_add_species_to_infos.rb
@@ -1,5 +1,14 @@
 class AddSpeciesToInfos < ActiveRecord::Migration[7.1]
   def change
+    add_column :infos, :method, :string
     add_column :infos, :species, :string
+    add_column :infos, :ages, :string
+    add_column :infos, :sex, :string
+    add_column :infos, :weight, :string
+    add_column :infos, :butchering_date, :date
+    add_column :infos, :processed_location, :string
+    add_column :infos, :processed_date, :date
+    add_column :infos, :processed_by, :string
+    
   end
 end
