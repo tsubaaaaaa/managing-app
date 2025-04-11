8518ed7396a754588749334921cea4439fff8ef6
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-04-11 16:49:38 +0900

    43 rev

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require rails-ujs
-
 
 import "@hotwired/turbo-rails"
 
-import { Turbo } from "@hotwired/turbo-rails"
 import Rails from "@rails/ujs"
 Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,2 +1,4 @@
-# Pin npm packages by running ./bin/importmap
-pin "rails-ujs", to: "rails-ujs.js"
+# config/importmap.rb
+
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@rails/ujs", to: "rails-ujs.js"
