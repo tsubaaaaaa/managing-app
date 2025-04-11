68678c831e8c70c18b115a91198ce11f6ef120df
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-04-11 13:40:45 +0900

    45th

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require rails-ujs
+//= require turbolinks
 
-
-import "@hotwired/turbo-rails"
 
 import "@hotwired/turbo-rails"
 import Rails from "@rails/ujs"
