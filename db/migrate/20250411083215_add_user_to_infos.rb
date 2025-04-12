88df121621b91d41ad3ff5e704f9802f49da5701
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-04-13 05:49:23 +0900

    45th rev

--- db/migrate/20250411083215_add_user_to_infos.rb
+++ db/migrate/20250411083215_add_user_to_infos.rb
@@ -1,5 +1,5 @@
 class AddUserToInfos < ActiveRecord::Migration[7.1]
   def change
-    add_reference :infos, :user, null: false, foreign_key: true
+    add_reference :infos, :user, foreign_key: true
   end
 end
