5026f185da5170c89f35a1b7606a671953681cf8
tsubaaaaaa <Ilovemake2886@gmail.com>
2025-04-17 01:21:27 +0900

    sort fixed (id → identifier)

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -50,8 +50,8 @@ class InfosController < ApplicationController
           "hunt_desc" => { Hunteddate: :desc },
           "processed_asc" => { processed_date: :asc },
           "processed_desc" => { processed_date: :desc },
-          "id_asc" => { id: :asc },
-          "id_desc" => { id: :desc }
+          "id_asc" => { identifier: :asc },
+          "id_desc" => { identifier: :desc }
         }
 
         if params[:sort].present? && sort_options.key?(params[:sort])
